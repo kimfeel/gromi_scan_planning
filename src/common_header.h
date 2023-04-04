77084d79214cf3e3b683ef04fa938bdff12c1e5d
pileun kim <pileun.kim@gmail.com>
2023-04-05 02:32:19 +0900

    Add files via upload

---
+++ src/common_header.h
@@ -0,0 +1,23 @@
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <geometry_msgs/PoseArray.h>
+#include <geometry_msgs/Point.h>
+#include <nav_msgs/Odometry.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/project_inliers.h>
+#include <pcl/ModelCoefficients.h>
+#include <iostream>
+
+typedef pcl::PointXYZI Ptype;
+typedef pcl::PointCloud<Ptype> PC;
+typedef sensor_msgs::PointCloud2 RosPC;
+typedef geometry_msgs::PoseArray PoseArray; 
+typedef geometry_msgs::Point RosPoint;
+typedef nav_msgs::Odometry Odom;
+
+const double RAIDUS_MAX = 30.;
+const double RAIDUS_MIN = 10.;
---
+++ src/planner.cpp
@@ -0,0 +1,82 @@
+#include <ros/ros.h>
+#include "frontier_detector.h"
+#include <geometry_msgs/PoseStamped.h>
+
+class Master{
+  public:
+    Master(ros::NodeHandle& nh);
+    ~Master();
+    int Loop();
+
+  private:
+    RosPoint robot_pos_;
+    PC::Ptr frontier_;
+    PC::Ptr world_;
+    ros::Subscriber lidar_sub_, pos_sub_;
+    ros::Publisher frontier_pub_, model_pub_;
+    FrontierDetector* detector_;
+    
+    void LidarCb(const RosPC::ConstPtr& msg);
+    void PosCb(const Odom& odom);
+};
+
+Master::~Master()
+{
+  delete detector_;
+}
+
+Master::Master(ros::NodeHandle& nh){
+  // Instantiate member objects
+  detector_ = new FrontierDetector();
+
+  // Create empty cloud
+  frontier_ = PC::Ptr (new PC);
+  world_ = PC::Ptr (new PC);
+
+  // Initialize subcribers and publishers
+  lidar_sub_ = nh.subscribe("velodyne_cloud_registered", 10, &Master::LidarCb, this);
+//  lidar_sub_ = nh.subscribe(registered_cloud_topic_name, 10, &Master::LidarCb, this);
+  pos_sub_ = nh.subscribe("aft_mapped_to_init", 1, &Master::PosCb, this);
+  frontier_pub_ = nh.advertise<RosPC>("frontier", 10);
+  model_pub_ = nh.advertise<RosPC>("world_model", 10);
+}
+
+int Master::Loop(){
+  while(ros::ok())
+  {
+    ros::Rate loop_rate(10);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }    
+}
+
+
+void Master::LidarCb(const RosPC::ConstPtr& imsg){
+  
+  // Get accumlated raw-frontier points
+  detector_->Detect(imsg, world_, frontier_);
+  detector_->ApplySensorModel(robot_pos_, frontier_);
+  detector_->ClusterPoints(frontier_);
+
+  // Convert pcl pointcloud to sensor_msg pointcloud2
+  RosPC omsg1, omsg2;
+  pcl::toROSMsg(*frontier_ , omsg1);
+  pcl::toROSMsg(*world_, omsg2);
+
+  // Publish to sensor frame 
+  omsg1.header = imsg->header;
+  omsg2.header = imsg->header;
+  frontier_pub_.publish(omsg1);
+  model_pub_.publish(omsg2);
+}
+
+void Master::PosCb(const Odom& odom){
+  robot_pos_ = odom.pose.pose.position;
+}
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "exploration_node");
+  ros::NodeHandle nh;
+  Master master(nh);
+  master.Loop();
+}
